53eebbecfbac284b640b508614029b1746c7cd71
milano12345 <dmilano857@gmail.com>
2019-08-23 13:44:34 -0500

    completed MVP

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
-import TabNav from "./components/TabNav.js";
+import {Route} from 'react-router-dom';
+
+import TabNav from './components/TabNav';
 import Header from "./components/Header.js";
+import EpisodesList from './components/EpisodeList';
+import CharacterList from './components/CharacterList';
+import LocationsList from './components/LocationsList';
+import Home from './components/Home';
 
 
 export default function App() {
   return (
     <main>
       <Header />
-      <TabNav />
-      
+      <TabNav/>
+      <Route exact path="/" component={Home} />
+      <Route path="/Episodes" component={EpisodesList} />
+      <Route path="/Characters" component={CharacterList} />
+      <Route path="/Locations" component={LocationsList} />
     </main>
   );
 }
--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -3,10 +3,14 @@ import { Tab } from 'semantic-ui-react';
 import CharacterList from './CharacterList';
 import LocationsList from './LocationsList';
 import EpisodesList from './EpisodeList';
+import Home from './Home';
+import {NavLink} from 'react-router-dom'
+
 const panes = [
-  { menuItem: 'Characters', render: () => <Tab.Pane><CharacterList/></Tab.Pane> },
-  { menuItem: 'Episodes', render: () => <Tab.Pane><EpisodesList/></Tab.Pane> },
-  { menuItem: 'Locations', render: () => <Tab.Pane><LocationsList/></Tab.Pane> },
+  { menuItem: 'Home', render: () => <Tab.Pane> <NavLink to="/"><Home/></NavLink></Tab.Pane> },
+  { menuItem: 'Characters', render: () => <Tab.Pane><NavLink to='CharactersList'><CharacterList/></NavLink></Tab.Pane> },
+  { menuItem: 'Episodes', render: () => <Tab.Pane><NavLink to='EpisodesList'><EpisodesList/></NavLink></Tab.Pane> },
+  { menuItem: 'Locations', render: () => <Tab.Pane><NavLink to='LocationsList'><LocationsList/></NavLink></Tab.Pane> },
 ]
 
 const TabNav = () => <Tab panes={panes} />
